f485ae0c2523813c9e940a71aedba57330ae0ebd
Samuel <samuel.muthaiga@student.moringaschool.com>
2022-11-24 01:38:00 +0300

    commit tree db7ffa5a323863667d44ac51441d569e6e7719f5

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+   
+return `This Saturday, I want to ${activity}!`
+}
+saturdayFun()
+
+ function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+mondayWork()
+
+ function wrapAdjective(adjective = "*") {
+    return function(message) {
+        return `You are ${adjective}${message}${adjective}!`
+    };
+} 
+wrapAdjective('*')('a hard worker')('*')
+wrapAdjective('||')('a dedicated programmer')('||')
